0e1e18df4bd6d11a47b057b471486e35aa0e24f1
alapolaa <zain.alapolaa@gmail.com>
2025-03-06 21:26:40 -0800

    menambahkan asset

--- lib/data/theme/img_string.dart
+++ lib/data/theme/img_string.dart
@@ -1,4 +1,34 @@
 class ImgString {
   static const String logo ='assets/images/logo.png';
   static const String sukses ='assets/images/sukses.png';
+  static const String banner1 ='assets/images/10.jpg';
+  static const String banner2 ='assets/images/11.jpg';
+  static const String banner3 ='assets/images/12.jpg';
+  static const String potensi ='assets/images/potensi.jpg';
+  static const String sejarah ='assets/images/desa-sejarah.jpg';
+  static const String berita1 ='assets/images/kelompok_tani.jpg';
+  static const String berita2 ='assets/images/penyaluran_blt.jpg';
+  static const String berita3 ='assets/images/berita3.jpeg';
+  static const String berita4 ='assets/images/berita4.jpeg';
+  static const String kegiatan1 ='assets/images/pertemuan.jpg';
+  static const String kegiatan2 ='assets/images/kegiatan2.jpeg';
+  static const String kegiatan3 ='assets/images/kegiatan3.jpeg';
+  static const String kegiatan4 ='assets/images/kegiatan4.jpeg';
+  static const String gallery1 ='assets/images/g1.jpeg';
+  static const String gallery2 ='assets/images/g2.jpeg';
+  static const String gallery3 ='assets/images/g3.jpeg';
+  static const String gallery4 ='assets/images/g4.jpeg';
+  static const String kepdes ='assets/images/kepdes.jpeg';
+  static const String wakil ='assets/images/wakil.jpeg';
+  static const String sekretaris1 ='assets/images/sekretaris1.jpeg';
+  static const String sekretaris2 ='assets/images/sekretaris1.jpeg';
+  static const String bendahara1 ='assets/images/ben1.jpeg';
+  static const String bendahara2 ='assets/images/ben2.jpeg';
+
+
+
+
+
+
+
 }
