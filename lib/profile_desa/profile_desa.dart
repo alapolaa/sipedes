e0f8e4838c7b8dac5d3a13ea95235f01d104773a
alapolaa <zain.alapolaa@gmail.com>
2025-03-08 18:46:19 -0800

    sejarah selesai

--- lib/profile_desa/profile_desa.dart
+++ lib/profile_desa/profile_desa.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sipedes/data/theme/theme.dart';
+import 'package:sipedes/footer.dart';
 import 'package:sipedes/profile_desa/sejarah/sejarah.dart';
 import 'package:sipedes/profile_desa/struktur_desa/struktur_desa.dart';
 import 'package:sipedes/profile_desa/visimisi/visimisi.dart';
@@ -32,18 +33,12 @@ class ProfileDesa extends StatelessWidget {
             ],
           ),
         ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 20.0.w,
-            vertical: 10.h,
-          ),
-          child: const TabBarView(
-            children: [
-              SejarahView(),
-              VisiMisiView(),
-              StrukturDesaView(),
-            ],
-          ),
+        body: const TabBarView(
+          children: [
+            SejarahView(),
+            VisiMisiView(),
+            StrukturDesaView(),
+          ],
         ),
       ),
     );
--- lib/profile_desa/sejarah/sejarah.dart
+++ lib/profile_desa/sejarah/sejarah.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sipedes/data/extension/extension.dart';
 import 'package:sipedes/data/theme/theme.dart';
+import 'package:sipedes/footer.dart';
 
 class SejarahView extends StatelessWidget {
   const SejarahView({super.key});
@@ -11,17 +12,31 @@ class SejarahView extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          Image.asset(
-            ImgString.gallery1,
-            fit: BoxFit.cover,
-            width: 360.w,
-            height: 270.h,
+          6.0.height,
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.w,
+            ),
+            child: Image.asset(
+              ImgString.gallery1,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 270.h,
+            ),
           ),
           6.0.height,
-          Text(
-            'Slampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi teks sejarah di sini)',
-            textAlign: TextAlign.justify,style: AppFont.duabelas,
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.w,
+            ),
+            child: Text(
+              'Slampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi tSlampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi tSlampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi tSlampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi tSlampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi tSlampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi tSlampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi tSlampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi tSlampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi tSlampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi tSlampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi tSlampar adalah desa yang benar ase kajs asjh dfjhjhdf jdsfjh dsfhdsfhj... (isi teks sejarah di sini)',
+              textAlign: TextAlign.justify,
+              style: AppFont.duabelas,
+            ),
           ),
+          100.0.height,
+          FooterScreen()
         ],
       ),
     );
