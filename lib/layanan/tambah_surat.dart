2b32068dbe9627f6400e3481f82d84ccfe1433b1
alapolaa <zain.alapolaa@gmail.com>
2025-03-29 12:46:04 -0700

    dimilosi

--- lib/layanan/tambah_surat.dart
+++ lib/layanan/tambah_surat.dart
@@ -1,8 +1,10 @@
-import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:image_picker/image_picker.dart';
+import 'package:file_picker/file_picker.dart';
+
+import 'package:intl/intl.dart';
+
 
 class TambahSurat extends StatefulWidget {
   @override
@@ -11,23 +13,22 @@ class TambahSurat extends StatefulWidget {
 
 class _TambahSuratState extends State<TambahSurat> {
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController namaController = TextEditingController();
-  final TextEditingController tempatLahirController = TextEditingController();
-  final TextEditingController tanggalLahirController = TextEditingController();
-  final TextEditingController nikController = TextEditingController();
-  final TextEditingController alamatController = TextEditingController();
-  final TextEditingController agamaController = TextEditingController();
-  final TextEditingController pekerjaanController = TextEditingController();
-  final TextEditingController keperluanController = TextEditingController();
+  TextEditingController namaController = TextEditingController();
+  TextEditingController nikController = TextEditingController();
+  TextEditingController alamatController = TextEditingController();
+  TextEditingController pekerjaanController = TextEditingController();
+  TextEditingController keperluanController = TextEditingController();
+  TextEditingController tempatLahirController = TextEditingController();
+  TextEditingController tanggalLahirController = TextEditingController();
 
-  File? _file;
-  final ImagePicker _picker = ImagePicker();
+  String? agama;
+  File? filePendukung;
 
   Future<void> _pickFile() async {
-    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
-    if (pickedFile != null) {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
       setState(() {
-        _file = File(pickedFile.path);
+        filePendukung = File(result.files.single.path!);
       });
     }
   }
@@ -35,61 +36,81 @@ class _TambahSuratState extends State<TambahSurat> {
   Future<void> _submitForm() async {
     if (!_formKey.currentState!.validate()) return;
 
-    var uri = Uri.parse("http://192.168.20.202/slampang/api/domisili.php");
-    var request = http.MultipartRequest("POST", uri);
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse('http://192.168.20.202/slampang/api/domisili.php'),
+    );
 
+    request.fields['id_pengguna'] = '13';
     request.fields['nama_lengkap'] = namaController.text;
-    request.fields['tempat_lahir'] = tempatLahirController.text;
-    request.fields['tanggal_lahir'] = tanggalLahirController.text;
     request.fields['nik'] = nikController.text;
     request.fields['alamat'] = alamatController.text;
-    request.fields['agama'] = agamaController.text;
+    request.fields['agama'] = agama ?? '';
     request.fields['pekerjaan'] = pekerjaanController.text;
     request.fields['keperluan'] = keperluanController.text;
+    request.fields['tempat_lahir'] = tempatLahirController.text;
+    request.fields['tanggal_lahir'] = tanggalLahirController.text;
 
-    if (_file != null) {
-      request.files.add(await http.MultipartFile.fromPath("file_pendukung", _file!.path));
+    if (filePendukung != null) {
+      request.files.add(
+        await http.MultipartFile.fromPath('file_pendukung', filePendukung!.path),
+      );
     }
 
     var response = await request.send();
-    var responseData = await response.stream.bytesToString();
-    var jsonResponse = jsonDecode(responseData);
 
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(jsonResponse['message'])),
-    );
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Pengajuan berhasil!')),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Gagal mengajukan surat!')),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Pengajuan Surat Domisili")),
+      appBar: AppBar(title: Text('Form Pengajuan Domisili')),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          child: ListView(
+          child: Column(
             children: [
-              TextFormField(controller: namaController, decoration: InputDecoration(labelText: "Nama Lengkap"), validator: (value) => value!.isEmpty ? "Harap isi nama" : null),
-              TextFormField(controller: tempatLahirController, decoration: InputDecoration(labelText: "Tempat Lahir")),
-              TextFormField(controller: tanggalLahirController, decoration: InputDecoration(labelText: "Tanggal Lahir")),
-              TextFormField(controller: nikController, decoration: InputDecoration(labelText: "NIK")),
-              TextFormField(controller: alamatController, decoration: InputDecoration(labelText: "Alamat")),
-              TextFormField(controller: agamaController, decoration: InputDecoration(labelText: "Agama")),
-              TextFormField(controller: pekerjaanController, decoration: InputDecoration(labelText: "Pekerjaan")),
-              TextFormField(controller: keperluanController, decoration: InputDecoration(labelText: "Keperluan")),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: _pickFile,
-                child: Text("Pilih File Pendukung"),
+              TextFormField(controller: namaController, decoration: InputDecoration(labelText: 'Nama Lengkap')),
+              TextFormField(controller: nikController, decoration: InputDecoration(labelText: 'NIK'), keyboardType: TextInputType.number),
+              TextFormField(controller: alamatController, decoration: InputDecoration(labelText: 'Alamat')),
+              DropdownButtonFormField<String>(
+                value: agama,
+                items: ['Islam', 'Kristen', 'Hindu', 'Budha'].map((e) => DropdownMenuItem(value: e, child: Text(e))).toList(),
+                onChanged: (val) => setState(() => agama = val),
+                decoration: InputDecoration(labelText: 'Agama'),
               ),
-              SizedBox(height: 10),
-              _file != null ? Text("File: ${_file!.path.split('/').last}") : Text("Tidak ada file dipilih"),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _submitForm,
-                child: Text("Kirim Pengajuan"),
+              TextFormField(controller: pekerjaanController, decoration: InputDecoration(labelText: 'Pekerjaan')),
+              TextFormField(controller: keperluanController, decoration: InputDecoration(labelText: 'Keperluan')),
+              TextFormField(
+                controller: tanggalLahirController,
+                decoration: InputDecoration(labelText: 'Tanggal Lahir'),
+                readOnly: true,
+                onTap: () async {
+                  DateTime? picked = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(1900),
+                    lastDate: DateTime.now(),
+                  );
+                  if (picked != null) {
+                    setState(() => tanggalLahirController.text = DateFormat('yyyy-MM-dd').format(picked));
+                  }
+                },
               ),
+              TextFormField(controller: tempatLahirController, decoration: InputDecoration(labelText: 'Tempat Lahir')),
+              ElevatedButton(onPressed: _pickFile, child: Text('Pilih File Pendukung')),
+              SizedBox(height: 16),
+              ElevatedButton(onPressed: _submitForm, child: Text('Submit')),
             ],
           ),
         ),
